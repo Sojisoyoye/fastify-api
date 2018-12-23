c752774766be0b70ace59aff87fb0d1cf5512638
Sojisoyoye <soji.soyoye@gmail.com>
2018-12-23 08:29:19 +0100

    Add documentation with swagger

--- src/index.js
+++ src/index.js
@@ -8,6 +8,12 @@ const mongoose = require('mongoose');
 
 const routes = require('./routes');
 
+// Import Swagger Options
+const swagger = require('./config/swagger');
+
+// Register Swagger
+fastify.register(require('fastify-swagger'), swagger.options);
+
 // Connect to DB
 mongoose.connect('mongodb://localhost/mycargarage').then(
     () => {console.log('MongoDB connected....')},
@@ -27,11 +33,12 @@ routes.forEach((route, index) => {
   // Run the server!
   const start = async () => {
     try {
-      await fastify.listen(3000)
-      fastify.log.info(`server listening on ${fastify.server.address().port}`)
+      await fastify.listen(3000);
+      fastify.swagger();
+      fastify.log.info(`server listening on ${fastify.server.address().port}`);
     } catch (err) {
-      fastify.log.error(err)
-      process.exit(1)
+      fastify.log.error(err);
+      process.exit(1);
     }
   }
   start()
